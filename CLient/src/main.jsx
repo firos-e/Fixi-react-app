2d5ffa1527ae638052a9cfe3c7e1672ce552ce83
firos-e <firoslatheef000@gmail.com>
2026-04-10 00:41:48 +0530

    fixi

--- CLient/src/main.jsx
+++ CLient/src/main.jsx
@@ -2,6 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import MainRout from './Routes/MainRout.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
